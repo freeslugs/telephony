93304d410a611b0d63fdb817c4b5a8a71149b94c
Gilad Penn <freeslugs@gmail.com>
2020-05-10 14:21:04 -0400

    fix bug

--- pages/_app.js
+++ pages/_app.js
@@ -32,12 +32,12 @@ export default function MyApp(props) {
         <title>Telephony</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content={description}>
+        <meta name="description" content={description} />
         <meta property="og:title" content="Telephony" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://get-telephony.now.sh/" />
         <meta property="og:image" content="https://get-telephony.now.sh/image.png" />
-        <meta property="og:description" content={description} />
+        <meta property="og:description" content={description}/>
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
