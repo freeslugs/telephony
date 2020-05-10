86f87f7f171af06360b21f089be1d95d0b5cf65b
Gilad Penn <freeslugs@gmail.com>
2020-05-10 13:13:22 -0400

    forgot a space

--- pages/Copyright.js
+++ pages/Copyright.js
@@ -5,7 +5,7 @@ const Copyright = () => <Typography variant="body2" color="textSecondary" align=
   {'Inspired by my mom. Copyright © '}
   <Link color="inherit" href="https://github.com/freeslugs">
     Gilad Penn
-  </Link>{' 2020.'}
+  </Link>{' 2020. '}
   <Link color="inherit" href="https://github.com/freeslugs/telephony">
   	100% open source
   </Link>
